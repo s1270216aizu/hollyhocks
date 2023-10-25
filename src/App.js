56973fa0e37d75f5b630a366165c1a8c5fe70ce3
Taiki Onodera <95903031+s1270216aizu@users.noreply.github.com>
2023-10-25 10:42:17 +0000

    こんにちは

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header bg-wed">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-wed-dark">
-          Welcome to <code>WED</code> Community.
+      <header className="App-header  Cat-color">
+        <img
+          src="https://img.benesse-cms.jp/pet-cat/item/image/normal/f3978ebc-9030-49e7-aa5e-4a370a955e1b.jpg?w=1200&h=1200&resize_type=cover&resize_mode=force"
+          className="App-logo"
+          alt="logo"
+        />
+        <p className=" great-purple">
+          ようこそ　<code>WED　</code> Communityへ
         </p>
         <a
           className="text-wed-dark underline"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     colors: {
-      wed: '#D2D2C6',
-      'wed-dark': '#6D695D',
+      wed: "#D2D2C6",
+      "wed-dark": "#6D695D",
+      "Cat-color": "#A274F3",
+      "great-purple": "#C71585",
     },
-    extend: {
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
